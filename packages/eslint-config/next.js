820479497aee177f9818dad41c00be8fdb069898
unknown <rahulpawar2001.rp@gmail.com>
2024-06-14 19:07:22 +0530

    updated packages

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -8,7 +8,7 @@ module.exports = {
     "eslint:recommended",
     "prettier",
     require.resolve("@vercel/style-guide/eslint/next"),
-    "turbo",
+    "eslint-config-turbo",
   ],
   globals: {
     React: true,
