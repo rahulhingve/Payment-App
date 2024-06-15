7e5b5ef12413cd4d5b7ae3875e63a586618d47a2
unknown <rahulpawar2001.rp@gmail.com>
2024-06-15 13:50:58 +0530

    complete project

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,37 +1,55 @@
 import express from "express";
-import db from "@repo/db/client"
+import db from "@repo/db/client";
 const app = express();
+
+app.use(express.json())
 
 app.post("/hdfcWebhook", async (req, res) => {
     //TODO: Add zod validation here?
-    const paymentInformation = {
+    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
+    const paymentInformation: {
+        token: string;
+        userId: string;
+        amount: string
+    } = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
     };
-    // Update balance in db, add txn
-    await db.balance.update({
-    where:{
-        userId:paymentInformation.userId
-    },
-    data:{
-        amount:{
-            increment:paymentInformation.amount
-        }
+
+    try {
+        await db.$transaction([
+            db.balance.updateMany({
+                where: {
+                    userId: Number(paymentInformation.userId)
+                },
+                data: {
+                    amount: {
+                        // You can also get this from your DB
+                        increment: Number(paymentInformation.amount)
+                    }
+                }
+            }),
+            db.onRampTransaction.updateMany({
+                where: {
+                    token: paymentInformation.token
+                }, 
+                data: {
+                    status: "Success",
+                }
+            })
+        ]);
+
+        res.json({
+            message: "Captured"
+        })
+    } catch(e) {
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
     }
-});
-await db.onRampTransaction.update({
-    where:{
-        token:paymentInformation.token,
-    },
-    data:{
-        status:"Success"
-        
-    }
-})
-res.status(200).json({
-    message:"captured"
+
 })
 
-
-})
+app.listen(3003);
